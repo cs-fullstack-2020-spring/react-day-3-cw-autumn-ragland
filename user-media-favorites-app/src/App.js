0cd261400cab604e58f94335b276b2861986ea8b
autumn-ragland <raglanda998@gmail.com>
2020-03-11 21:59:00 -0500

    set up

---
+++ user-media-favorites-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>User Media Favorites App</h1>
+    </div>
+  );
+}
+
+export default App;
